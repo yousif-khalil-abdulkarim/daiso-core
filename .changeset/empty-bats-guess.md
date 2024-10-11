fad301c2bb1495c45a399fffca6f73895dded4da
yousif-khalil-abdulkarim <118832973+yousif-khalil-abdulkarim@users.noreply.github.com>
2024-10-11 20:31:06 +0200

    Removed documentation for internal functions (#43)

---
+++ .changeset/empty-bats-guess.md
@@ -0,0 +1,5 @@
+---
+"@daiso-tech/core": patch
+---
+
+Removed documentation for internal functions
--- src/_shared/utilities.ts
+++ src/_shared/utilities.ts
@@ -1,5 +1,8 @@
 import { AsyncLazyable, Lazyable } from "@/_shared/types";
 
+/**
+ * @internal
+ */
 export function simplifyLazyable<TValue>(lazyable: Lazyable<TValue>): TValue {
     if (typeof lazyable === "function") {
         const getValue = lazyable as () => TValue;
@@ -7,6 +10,9 @@ export function simplifyLazyable<TValue>(lazyable: Lazyable<TValue>): TValue {
     }
     return lazyable;
 }
+/**
+ * @internal
+ */
 export async function simplifyAsyncLazyable<TValue>(
     lazyable: AsyncLazyable<TValue>,
 ): Promise<TValue> {
