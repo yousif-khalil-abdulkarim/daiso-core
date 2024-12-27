7ff02cf7251f32ffae34e2ffa466574d34a94735
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2024-12-27 03:54:17 +0100

    Added validation utilities

--- src/utilities/_module.ts
+++ src/utilities/_module.ts
@@ -2,3 +2,4 @@ export * from "@/utilities/_module";
 export * from "@/utilities/async/_module";
 export * from "@/utilities/async-iterable/_module";
 export * from "@/utilities/time-span/_module";
+export * from "@/utilities/validation/_module";
