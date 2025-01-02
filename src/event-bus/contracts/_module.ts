5f6a3f3f1e2a282337dfa895bb86c9c097466a07
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2025-01-02 06:31:29 +0100

    Added IEventBus and IEventBusAdapter contracts

---
+++ src/event-bus/contracts/_module.ts
@@ -0,0 +1,3 @@
+export * from "@/event-bus/contracts/_shared";
+export * from "@/event-bus/contracts/event-bus.contract";
+export * from "@/event-bus/contracts/event-bus-adapter.contract";
---
+++ src/event-bus/contracts/_shared.ts
@@ -0,0 +1,52 @@
+/**
+ * @module EventBus
+ */
+
+import type { Promisable } from "@/_shared/types";
+
+export type IBaseEvent = {
+    type: string;
+};
+export type Listener<TEvent extends IBaseEvent> = (
+    event: TEvent,
+) => Promisable<void>;
+
+/**
+ * @group Errors
+ */
+export class EventBusError extends Error {
+    constructor(message: string, cause?: unknown) {
+        super(message, { cause });
+        this.name = EventBusError.name;
+    }
+}
+
+/**
+ * @group Errors
+ */
+export class RemoveListenerEventBusError extends Error {
+    constructor(message: string, cause?: unknown) {
+        super(message, { cause });
+        this.name = RemoveListenerEventBusError.name;
+    }
+}
+
+/**
+ * @group Errors
+ */
+export class AddListenerEventBusError extends Error {
+    constructor(message: string, cause?: unknown) {
+        super(message, { cause });
+        this.name = AddListenerEventBusError.name;
+    }
+}
+
+/**
+ * @group Errors
+ */
+export class DispatchEventBusError extends Error {
+    constructor(message: string, cause?: unknown) {
+        super(message, { cause });
+        this.name = DispatchEventBusError.name;
+    }
+}
