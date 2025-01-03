ea5766db7c9f1d89ac5b3c7de277767af49d1f2a
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2025-01-03 23:54:48 +0100

    Added IEventBusManager contract

--- src/event-bus/contracts/_module.ts
+++ src/event-bus/contracts/_module.ts
@@ -1,3 +1,4 @@
 export * from "@/event-bus/contracts/_shared";
 export * from "@/event-bus/contracts/event-bus.contract";
 export * from "@/event-bus/contracts/event-bus-adapter.contract";
+export * from "@/event-bus/contracts/event-bus-manager.contract";
