9e0583fd599e87dce3954e988af648e70fc05238
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2025-01-05 01:44:49 +0100

    renamed eventBusTestSuite to eventBusAdapterTestSuite

--- src/event-bus/implementations/memory-event-bus-adapter/memory-event-bus-adapter.test.ts
+++ src/event-bus/implementations/memory-event-bus-adapter/memory-event-bus-adapter.test.ts
@@ -1,9 +1,9 @@
 import { describe, test, beforeEach, expect } from "vitest";
-import { eventBusTestSuite } from "@/event-bus/implementations/_shared/test-utilities/event-bus-adapter.test-suite";
+import { eventBusAdapterTestSuite } from "@/event-bus/implementations/_shared/test-utilities/event-bus-adapter.test-suite";
 import { MemoryEventBusAdapter } from "@/event-bus/implementations/memory-event-bus-adapter/memory-event-bus-adapter";
 
 describe("class: MemoryEventBusAdapter", () => {
-    eventBusTestSuite({
+    eventBusAdapterTestSuite({
         createAdapter: () => new MemoryEventBusAdapter(),
         test,
         beforeEach,
