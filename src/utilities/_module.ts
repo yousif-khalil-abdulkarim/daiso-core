71ddd4d5b536d564372cab0888b2a4a2eacfa88f
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2025-01-11 15:39:08 +0100

    Added global errors

--- src/utilities/_module.ts
+++ src/utilities/_module.ts
@@ -2,3 +2,4 @@ export * from "@/utilities/async/_module";
 export * from "@/utilities/backof-policies/_module";
 export * from "@/utilities/time-span/_module";
 export * from "@/utilities/validation/_module";
+export * from "@/utilities/global-errors";
