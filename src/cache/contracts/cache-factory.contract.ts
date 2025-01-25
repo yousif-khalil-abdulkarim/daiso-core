c71383e623369f767223b5c7022e6159927dc540
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2025-01-25 17:56:53 +0100

    Updated the ICacheFactory contract

--- src/cache/contracts/cache-factory.contract.ts
+++ src/cache/contracts/cache-factory.contract.ts
@@ -5,21 +5,21 @@
 import type { IGroupableCache } from "@/cache/contracts/cache.contract";
 import {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    UnregisteredDriverError,
+    UnregisteredAdapterError,
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    DefaultDriverNotDefinedError,
+    DefaultAdapterNotDefinedError,
 } from "@/utilities/errors";
 
 /**
  * The <i>ICacheFactory</i> contract makes it easy to configure and switch between different <i>{@link IGroupableCache}</i> dynamically.
  * @group Contracts
  */
-export type ICacheFactory<TDrivers extends string = string> = {
+export type ICacheFactory<TAdapters extends string = string> = {
     /**
-     * The <i>use</i> method will throw an error if you provide it unregisted driver.
-     * If no default driver is defined an error will be thrown by <i>use</i> method.
-     * @throws {UnregisteredDriverError} {@link UnregisteredDriverError}
-     * @throws {DefaultDriverNotDefinedError} {@link DefaultDriverNotDefinedError}
+     * The <i>use</i> method will throw an error if you provide it unregisted adapter.
+     * If no default adapter is defined an error will be thrown by <i>use</i> method.
+     * @throws {UnregisteredAdapterError} {@link UnregisteredAdapterError}
+     * @throws {DefaultAdapterNotDefinedError} {@link DefaultAdapterNotDefinedError}
      * @example
      * ```ts
      * import { type ICacheFactory } from "@daiso-tech/core";
@@ -28,7 +28,7 @@ export type ICacheFactory<TDrivers extends string = string> = {
      * // Asume the inputed cacheFactory has registered both a memory and Redis ICacheAdapter.
      * // The memory ICacheAdapter adapter is the default.
      * async function main(cacheFactory: ICacheFactory): Promise<void> {
-     *   // Will add key using the default driver
+     *   // Will add key using the default adapter
      *   await cacheFactory
      *     .use()
      *     .add("a", 1);
@@ -39,5 +39,5 @@ export type ICacheFactory<TDrivers extends string = string> = {
      * }
      * ```
      */
-    use<TType = unknown>(driverName?: TDrivers): IGroupableCache<TType>;
+    use<TType = unknown>(adapterName?: TAdapters): IGroupableCache<TType>;
 };
