a0cf2af50dfc336c7147502d4289196f9bc50850
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2025-01-25 18:03:55 +0100

    Updated the SqliteCacheAdapter class

---
+++ src/cache/implementations/adapters/sqlite-cache-adapter/sqlite-cache-adapter-settings.ts
@@ -0,0 +1,93 @@
+/**
+ * @module Cache
+ */
+
+import type { ISerde } from "@/serde/contracts/_module";
+import type { TimeSpan, OneOrMore, IBuildable } from "@/utilities/_module";
+import { type Database } from "better-sqlite3";
+
+/**
+ * @group Adapters
+ */
+export type SqliteCacheAdapterSettings = {
+    database: Database;
+    tableName?: string;
+    serde: ISerde<string>;
+    enableTransactions?: boolean;
+    expiredKeysRemovalInterval?: TimeSpan;
+    shouldRemoveExpiredKeys?: boolean;
+    rootGroup: OneOrMore<string>;
+};
+
+/**
+ * The <i>SqliteCacheAdapterSettingsBuilder</i> is an immutable builder class, meaning that each method invocation creates a new instance.
+ * @group Adapters
+ */
+export class SqliteCacheAdapterSettingsBuilder<
+    TSettings extends Partial<SqliteCacheAdapterSettings>,
+> implements IBuildable<SqliteCacheAdapterSettings>
+{
+    constructor(private readonly settings: TSettings = {} as TSettings) {}
+
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    setDatabase(database: Database) {
+        return new SqliteCacheAdapterSettingsBuilder({
+            ...this.settings,
+            database,
+        });
+    }
+
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    setTableName(name: string) {
+        return new SqliteCacheAdapterSettingsBuilder({
+            ...this.settings,
+            tableName: name,
+        });
+    }
+
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    setSerde(serde: ISerde<string>) {
+        return new SqliteCacheAdapterSettingsBuilder({
+            ...this.settings,
+            serde,
+        });
+    }
+
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    setEnableTransactions(enable: boolean) {
+        return new SqliteCacheAdapterSettingsBuilder({
+            ...this.settings,
+            enableTransactions: enable,
+        });
+    }
+
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    setExpiredKeysRemovalInterval(interval: TimeSpan) {
+        return new SqliteCacheAdapterSettingsBuilder({
+            ...this.settings,
+            expiredKeysRemovalInterval: interval,
+        });
+    }
+
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    setShouldRemoveExpiredKeys(shouldRemove: boolean) {
+        return new SqliteCacheAdapterSettingsBuilder({
+            ...this.settings,
+            shouldRemoveExpiredKeys: shouldRemove,
+        });
+    }
+
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    setRootGroup(group: OneOrMore<string>) {
+        return new SqliteCacheAdapterSettingsBuilder({
+            ...this.settings,
+            rootGroup: group,
+        });
+    }
+
+    build(
+        this: SqliteCacheAdapterSettingsBuilder<SqliteCacheAdapterSettings>,
+    ): SqliteCacheAdapterSettings {
+        return this.settings;
+    }
+}
--- src/cache/implementations/adapters/sqlite-cache-adapter/sqlite-cache-adapter.ts
+++ src/cache/implementations/adapters/sqlite-cache-adapter/sqlite-cache-adapter.ts
@@ -3,7 +3,6 @@
  */
 
 import { type ICacheAdapter } from "@/cache/contracts/cache-adapter.contract";
-import type { ISerde } from "@/serde/contracts/_module";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { SuperJsonSerde } from "@/serde/implementations/_module";
 import type {
@@ -12,22 +11,11 @@ import type {
     IDeinitizable,
     OneOrMore,
 } from "@/utilities/_module";
-import { type Database } from "better-sqlite3";
 import { KyselySqliteCacheAdapter } from "@/cache/implementations/adapters/kysely-sqlite-cache-adapter/_module";
 import { Kysely, SqliteDialect } from "kysely";
 import { KyselyTableNameTransformerPlugin } from "@/utilities/_module";
-
-/**
- * @group Adapters
- */
-export type SqliteStorageAdapterSettings = {
-    tableName?: string;
-    serde: ISerde<string>;
-    enableTransactions?: boolean;
-    expiredKeysRemovalInterval?: TimeSpan;
-    shouldRemoveExpiredKeys?: boolean;
-    rootGroup: OneOrMore<string>;
-};
+import type { SqliteCacheAdapterSettings } from "@/cache/implementations/adapters/sqlite-cache-adapter/sqlite-cache-adapter-settings";
+import { SqliteCacheAdapterSettingsBuilder } from "@/cache/implementations/adapters/sqlite-cache-adapter/sqlite-cache-adapter-settings";
 
 /**
  * To utilize the <i>SqliteCacheAdapter</i>, you must install the <i>"better-sqlite3"</i> package and supply a <i>{@link ISerde | ISerde<string> }</i>, such as <i>{@link SuperJsonSerde}</i>.
@@ -36,6 +24,28 @@ export type SqliteStorageAdapterSettings = {
 export class SqliteCacheAdapter<TType = unknown>
     implements ICacheAdapter<TType>, IInitizable, IDeinitizable
 {
+    /**
+     * @example
+     * ```ts
+     * import { LibsqlCacheAdapter, SuperJsonSerde } from "@daiso-tech/core";
+     * import Sqlite from "better-sqlite3";
+     *
+     * const cacheAdapter = new SqliteCacheAdapter(
+     *   SqliteCacheAdapter
+     *     .settings()
+     *     .setDatabase(new Sqlite("local.db"))
+     *     .setSerde(new SuperJsonSerde())
+     *     .setRootGroup("@global")
+     *     .build()
+     * );
+     * ```
+     */
+    static settings<
+        TSettings extends Partial<SqliteCacheAdapterSettings>,
+    >(): SqliteCacheAdapterSettingsBuilder<TSettings> {
+        return new SqliteCacheAdapterSettingsBuilder();
+    }
+
     private readonly cacheAdapter: KyselySqliteCacheAdapter<TType>;
 
     /**
@@ -44,25 +54,24 @@ export class SqliteCacheAdapter<TType = unknown>
      * import { SqliteCacheAdapter, SuperJsonSerde } from "@daiso-tech/core";
      * import Sqlite from "better-sqlite3";
      *
-     * const client = new Sqlite("local.db");
+     * const database = new Sqlite("local.db");
      * const serde = new SuperJsonSerde();
-     * const cacheAdapter = new SqliteCacheAdapter(client, {
+     * const cacheAdapter = new SqliteCacheAdapter({
+     *   database,
      *   serde,
      *   rootGroup: "@global"
      * });
      *
      * (async () => {
-     *   // You only need to call it once before using the adapter.
      *   await cacheAdapter.init();
      *   await cacheAdapter.add("a", 1);
-     *
-     *   // Will remove the cache
      *   await cacheAdapter.deInit();
      * })();
      * ```
      */
-    constructor(database: Database, settings: SqliteStorageAdapterSettings) {
+    constructor(settings: SqliteCacheAdapterSettings) {
         const {
+            database,
             tableName = "cache",
             serde,
             enableTransactions = false,
@@ -71,10 +80,10 @@ export class SqliteCacheAdapter<TType = unknown>
             rootGroup,
         } = settings;
 
-        this.cacheAdapter = new KyselySqliteCacheAdapter(
-            new Kysely({
+        this.cacheAdapter = new KyselySqliteCacheAdapter({
+            db: new Kysely({
                 dialect: new SqliteDialect({
-                    database,
+                    database: database,
                 }),
                 plugins: [
                     new KyselyTableNameTransformerPlugin({
@@ -82,14 +91,12 @@ export class SqliteCacheAdapter<TType = unknown>
                     }),
                 ],
             }),
-            {
-                serde,
-                enableTransactions,
-                expiredKeysRemovalInterval,
-                shouldRemoveExpiredKeys,
-                rootGroup,
-            },
-        );
+            serde,
+            enableTransactions,
+            expiredKeysRemovalInterval,
+            shouldRemoveExpiredKeys,
+            rootGroup,
+        });
     }
 
     getGroup(): string {
@@ -104,10 +111,16 @@ export class SqliteCacheAdapter<TType = unknown>
         await this.cacheAdapter.removeExpiredKeys();
     }
 
+    /**
+     * Removes the table where the cache values are stored and removes the table indexes.
+     */
     async deInit(): Promise<void> {
         await this.cacheAdapter.deInit();
     }
 
+    /**
+     * Creates the table where the cache values are stored and it's related indexes.
+     */
     async init(): Promise<void> {
         await this.cacheAdapter.init();
     }
