c0f9056f04b433a8c5e63dc05ece6d4d5a3abd38
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2025-02-06 00:15:22 +0100

    Removed RedisCacheAadapterSettingsBuilder and static RedisCacheAdapter.settings method

--- src/cache/implementations/adapters/redis-cache-adapter/_module.ts
+++ src/cache/implementations/adapters/redis-cache-adapter/_module.ts
@@ -1,2 +1,1 @@
-export * from "@/cache/implementations/adapters/redis-cache-adapter/redis-cache-adapter-settings";
 export * from "@/cache/implementations/adapters/redis-cache-adapter/redis-cache-adapter";
