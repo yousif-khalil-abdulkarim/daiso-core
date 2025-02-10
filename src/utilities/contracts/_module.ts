2b3ffe84884b2c990c358a2406c5db6081604043
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2025-02-10 01:04:28 +0100

    Removed ISerdeRegistrable contract and changed the LockProvider class

--- src/utilities/contracts/_module.ts
+++ src/utilities/contracts/_module.ts
@@ -2,4 +2,3 @@ export * from "@/utilities/contracts/serialized-error.contract";
 export * from "@/utilities/contracts/buildable.contract";
 export * from "@/utilities/contracts/deinitizable.contract";
 export * from "@/utilities/contracts/initizable.contract";
-export * from "@/utilities/contracts/serde-registrable.contract";
