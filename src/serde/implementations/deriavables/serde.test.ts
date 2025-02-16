07b18b247ecab4cbb8b372da10401dc2caf4e067
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2025-02-16 03:27:15 +0100

    Added derivable Serde class

---
+++ src/serde/implementations/deriavables/serde.test.ts
@@ -0,0 +1,12 @@
+import { flexibleSerdeTestSuite } from "@/serde/implementations/_shared/flexible-serde.test-suite";
+import { describe, test, expect } from "vitest";
+import { Serde } from "@/serde/implementations/deriavables/serde";
+import { SuperJsonSerdeAdapter } from "@/serde/implementations/_module";
+
+describe("class: Serde", () => {
+    flexibleSerdeTestSuite({
+        create: () => new Serde(new SuperJsonSerdeAdapter()),
+        test,
+        expect,
+    });
+});
