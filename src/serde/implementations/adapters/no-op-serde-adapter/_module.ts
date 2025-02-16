43a914184ba4ad35d1e5f5aa82aca33d2ab15629
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2025-02-16 03:26:08 +0100

    Rewrote NoOpSerde and renamed it NoOpSerdeAdapter

---
+++ src/serde/implementations/adapters/no-op-serde-adapter/_module.ts
@@ -0,0 +1,1 @@
+export * from "@/serde/implementations/adapters/no-op-serde-adapter/no-op-serde-adapter";
---
+++ src/serde/implementations/adapters/no-op-serde-adapter/no-op-serde-adapter.ts
@@ -0,0 +1,25 @@
+import type {
+    IFlexibleSerdeAdapter,
+    ISerdeTransformerAdapter,
+} from "@/serde/contracts/flexible-serde-adapter.contract";
+
+export class NoOpSerdeAdapter<TSerializedValue>
+    implements IFlexibleSerdeAdapter<TSerializedValue>
+{
+    serialize<TValue>(value: TValue): TSerializedValue {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        return value as any;
+    }
+
+    deserialize<TValue>(serializedValue: TSerializedValue): TValue {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        return serializedValue as any;
+    }
+
+    registerCustom<TCustomSerialized, TCustomDeserialized>(
+        _transformer: ISerdeTransformerAdapter<
+            TCustomSerialized,
+            TCustomDeserialized
+        >,
+    ): void {}
+}
