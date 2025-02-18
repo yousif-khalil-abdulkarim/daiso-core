cbcfcc7d0bbed8f7353fdedf1f34576122528620
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2025-02-18 02:34:38 +0100

    Removed unused utility contract

--- src/utilities/contracts/_module.ts
+++ src/utilities/contracts/_module.ts
@@ -1,4 +1,3 @@
 export * from "@/utilities/contracts/serialized-error.contract.js";
-export * from "@/utilities/contracts/buildable.contract.js";
 export * from "@/utilities/contracts/deinitizable.contract.js";
 export * from "@/utilities/contracts/initizable.contract.js";
