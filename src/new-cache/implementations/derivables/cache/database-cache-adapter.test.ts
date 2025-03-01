5c6798cf7d622a20b3003d298d6240e4ea094844
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2025-03-01 16:23:05 +0100

    Added DatabaseCacheAdapter class

---
+++ src/new-cache/implementations/derivables/cache/database-cache-adapter.test.ts
@@ -0,0 +1,33 @@
+import { afterEach, beforeEach, describe, expect, test } from "vitest";
+import { cacheAdapterTestSuite } from "@/new-cache/implementations/test-utilities/_module-exports.js";
+import { SqliteCacheAdapter } from "@/new-cache/implementations/adapters/_module-exports.js";
+import Sqlite, { type Database } from "better-sqlite3";
+import { Serde } from "@/serde/implementations/derivables/_module-exports.js";
+import { SuperJsonSerdeAdapter } from "@/serde/implementations/adapters/_module-exports.js";
+import { DatabaseCacheAdapter } from "@/new-cache/implementations/derivables/_module-exports.js";
+
+describe("class: DatabaseCacheAdapter", () => {
+    let database: Database;
+    beforeEach(() => {
+        database = new Sqlite(":memory:");
+    });
+    afterEach(() => {
+        database.close();
+    });
+    cacheAdapterTestSuite({
+        createAdapter: async () => {
+            const adapter = new SqliteCacheAdapter({
+                database: database,
+                tableName: "custom_table",
+                shouldRemoveExpiredKeys: false,
+                serde: new Serde(new SuperJsonSerdeAdapter()),
+            });
+            await adapter.init();
+            return new DatabaseCacheAdapter(adapter);
+        },
+        test,
+        beforeEach,
+        expect,
+        describe,
+    });
+});
---
+++ src/new-cache/implementations/derivables/cache/database-cache-adapter.ts
@@ -0,0 +1,129 @@
+/**
+ * @module Cache
+ */
+
+import {
+    type IDatabaseCacheAdapter,
+    type ICacheAdapter,
+    TypeCacheError,
+} from "@/new-cache/contracts/_module-exports.js";
+import type { TimeSpan } from "@/utilities/_module-exports.js";
+
+/**
+ * @internal
+ */
+export class DatabaseCacheAdapter<TType = unknown>
+    implements ICacheAdapter<TType>
+{
+    constructor(private readonly adapter: IDatabaseCacheAdapter<TType>) {}
+
+    async get(key: string): Promise<TType | null> {
+        const data = await this.adapter.find(key);
+        if (data === null) {
+            return null;
+        }
+        const { expiration, value } = data;
+        if (expiration === null) {
+            return value;
+        }
+        const hasExpired = expiration.getTime() <= new Date().getTime();
+        if (hasExpired) {
+            await this.adapter.removeExpiredMany([key]);
+            return null;
+        }
+        return value;
+    }
+
+    async getAndRemove(key: string): Promise<TType | null> {
+        const value = await this.get(key);
+        if (value !== null) {
+            await this.removeMany([key]);
+        }
+        return value;
+    }
+
+    async add(
+        key: string,
+        value: TType,
+        ttl: TimeSpan | null,
+    ): Promise<boolean> {
+        try {
+            await this.adapter.insert({
+                key,
+                value,
+                expiration: ttl?.toEndDate() ?? null,
+            });
+            return true;
+        } catch {
+            const result = await this.adapter.updateExpired({
+                expiration: ttl?.toEndDate() ?? null,
+                key,
+                value,
+            });
+            return result > 0;
+        }
+    }
+
+    async put(
+        key: string,
+        value: TType,
+        ttl: TimeSpan | null,
+    ): Promise<boolean> {
+        const data = await this.adapter.upsert({
+            key,
+            value,
+            expiration: ttl?.toEndDate() ?? null,
+        });
+        if (data === null) {
+            return false;
+        }
+        const { expiration } = data;
+        if (expiration === null) {
+            return true;
+        }
+        const hasExpired = expiration.getTime() <= new Date().getTime();
+        return !hasExpired;
+    }
+
+    async update(key: string, value: TType): Promise<boolean> {
+        const result = await this.adapter.updateUnexpired({
+            key,
+            value,
+        });
+        return result > 0;
+    }
+
+    async increment(key: string, value: number): Promise<boolean> {
+        try {
+            const result = await this.adapter.incrementUnexpired({
+                key,
+                value,
+            });
+            return result > 0;
+        } catch (error: unknown) {
+            if (error instanceof TypeCacheError) {
+                throw error;
+            }
+            throw new TypeCacheError(
+                `Unable to increment or decrement none number type key "${key}"`,
+                error,
+            );
+        }
+    }
+
+    async removeMany(keys: string[]): Promise<boolean> {
+        const [promiseResult] = await Promise.allSettled([
+            this.adapter.removeUnexpiredMany(keys),
+            this.adapter.removeExpiredMany(keys),
+        ]);
+        if (promiseResult.status === "rejected") {
+            throw promiseResult.reason;
+        }
+        const { value: result } = promiseResult;
+        return result > 0;
+    }
+
+    async removeByKeyPrefix(prefix: string): Promise<void> {
+        await this.adapter.removeByKeyPrefix(prefix);
+    }
+}
