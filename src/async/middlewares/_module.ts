90af17a42cc297742e0b5c58a5e6c8a2e8827409
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2025-04-02 02:33:20 +0200

    Updated exports

--- src/async/middlewares/_module.ts
+++ src/async/middlewares/_module.ts
@@ -1,4 +1,5 @@
+export * from "@/async/middlewares/dynamic/_module.js";
 export * from "@/async/middlewares/fallback/_module.js";
-export * from "@/async/middlewares/observation/_module.js";
+export * from "@/async/middlewares/observe/_module.js";
 export * from "@/async/middlewares/retry/_module.js";
 export * from "@/async/middlewares/timeout/_module.js";
