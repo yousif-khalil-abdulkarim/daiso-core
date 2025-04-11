7125f72ca7006fdd7df4fb29fdb7738e7a6dbe07
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2025-04-11 03:58:11 +0200

    Added PromiseQueue utility class

---
+++ src/async/utilities/promise-queue/promise-queue.ts
@@ -0,0 +1,164 @@
+/**
+ * @module Async
+ */
+
+import type { TimeSpan } from "@/utilities/_module-exports.js";
+import {
+    type InvokableFn,
+    type Promisable,
+    type Result,
+} from "@/utilities/_module-exports.js";
+import { LazyPromise } from "@/async/utilities/_module.js";
+import { v4 } from "uuid";
+import { CapacityFullAsyncError } from "@/async/async.errors.js";
+
+/**
+ * @internal
+ */
+class Queue<TValue> {
+    private readonly array: TValue[] = [];
+
+    size(): number {
+        return this.array.length;
+    }
+
+    enqueue(value: TValue): void {
+        this.array.push(value);
+    }
+
+    dequeue(): TValue | null {
+        return this.array.shift() ?? null;
+    }
+}
+
+/**
+ * @internal
+ */
+type PromiseQueueItem<TValue> = {
+    id: string;
+    func: InvokableFn<[signal: AbortSignal], Promisable<TValue>>;
+    signal: AbortSignal;
+};
+
+/**
+ * @internal
+ */
+type PromiseQueueListener<TValue> = InvokableFn<
+    [result: Result<TValue, unknown>]
+>;
+
+/**
+ * @internal
+ */
+export type PromiseQueueSettings = {
+    maxConcurrency: number;
+    maxCapacity: number | null;
+    interval: TimeSpan;
+};
+
+/**
+ * @internal
+ */
+export class PromiseQueue {
+    private readonly queue = new Queue<PromiseQueueItem<unknown>>();
+
+    private readonly listeners = new Map<string, PromiseQueueListener<any>>();
+
+    constructor(private readonly settings: PromiseQueueSettings) {
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        this.start();
+    }
+
+    private async start(): Promise<void> {
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition, no-constant-condition
+        while (true) {
+            await Promise.all(
+                this.getItems().map((item) => this.process(item)),
+            );
+            await LazyPromise.delay(this.settings.interval);
+        }
+    }
+
+    private getItems<TValue>(): PromiseQueueItem<TValue>[] {
+        const values: PromiseQueueItem<TValue>[] = [];
+        for (let i = 0; i < this.settings.maxConcurrency; i++) {
+            const item =
+                this.queue.dequeue() as PromiseQueueItem<TValue> | null;
+            if (item === null) {
+                continue;
+            }
+            values.push(item);
+        }
+        return values;
+    }
+
+    private async process<TValue>(
+        item: PromiseQueueItem<TValue>,
+    ): Promise<void> {
+        const listener = this.listeners.get(item.id);
+
+        try {
+            if (item.signal.aborted) {
+                listener?.([null, item.signal.reason]);
+                return;
+            }
+            const value = await item.func(item.signal);
+            listener?.([value, null]);
+        } catch (error: unknown) {
+            listener?.([null, error]);
+
+            throw error;
+        }
+    }
+
+    private enqueue<TValue>(
+        func: InvokableFn<[signal: AbortSignal], Promisable<TValue>>,
+        signal: AbortSignal,
+    ): string {
+        const id = v4();
+        if (this.settings.maxCapacity === null) {
+            this.queue.enqueue({
+                id,
+                func,
+                signal,
+            });
+        } else if (this.queue.size() <= this.settings.maxCapacity) {
+            this.queue.enqueue({ id, func, signal });
+        } else {
+            throw new CapacityFullAsyncError(
+                `Max capacity reached, ${String(this.settings.maxCapacity)} items allowed.`,
+            );
+        }
+        return id;
+    }
+
+    private listenOnce<TValue>(
+        id: string,
+        listener: PromiseQueueListener<TValue>,
+    ): void {
+        this.listeners.set(id, listener);
+    }
+
+    private asPromise<TValue>(id: string): Promise<TValue> {
+        return new Promise<TValue>((resolve, reject) => {
+            this.listenOnce(id, ([value, error]) => {
+                if (value === null) {
+                    // eslint-disable-next-line @typescript-eslint/prefer-promise-reject-errors
+                    reject(error);
+                    return;
+                }
+                resolve(value as TValue);
+            });
+        });
+    }
+
+    /**
+     * @throws {CapacityFullAsyncError} {@link CapacityFullAsyncError}
+     */
+    add<TValue>(
+        func: InvokableFn<[signal: AbortSignal], Promisable<TValue>>,
+        signal: AbortSignal,
+    ): Promise<TValue> {
+        return this.asPromise(this.enqueue(func, signal));
+    }
+}
