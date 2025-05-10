04fd5e53e232ede54e77f9f82e255be8e892fe01
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2025-05-10 05:13:38 +0200

    Added documentation website

---
+++ website/docusaurus.config.ts
@@ -0,0 +1,113 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+    title: "@daiso-tech/core",
+    tagline: "A flexible full featured backend library - build faster and worry less.",
+        // "Build faster. Worry less. A flexible library for core backend functionalities.",
+    favicon: "img/favicon.ico",
+
+    // Set the production url of your site here
+    url: "https://your-docusaurus-site.example.com",
+    
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: "/",
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: "facebook", // Usually your GitHub org/user name.
+    projectName: "docusaurus", // Usually your repo name.
+
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en"],
+    },
+
+    presets: [
+        [
+            "classic",
+            {
+                docs: {
+                    sidebarPath: "./sidebars.ts",
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    // editUrl:
+                    //     "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+                },
+                blog: {
+                    showReadingTime: true,
+                    feedOptions: {
+                        type: ["rss", "atom"],
+                        xslt: true,
+                    },
+                    onInlineTags: "warn",
+                    onInlineAuthors: "warn",
+                    onUntruncatedBlogPosts: "warn",
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    // editUrl:
+                    //     "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+                    // Useful options to enforce blogging best practices
+                },
+                theme: {
+                    customCss: "./src/css/custom.css",
+                },
+            } satisfies Preset.Options,
+        ],
+    ],
+
+    themeConfig: {
+        // Replace with your project's social card
+        image: "img/docusaurus-social-card.jpg",
+        navbar: {
+            title: "@daiso-tech/core",
+            logo: {
+                alt: "@daiso-tech/core Logo",
+                src: "img/logo.svg",
+            },
+            items: [
+                {
+                    type: "docSidebar",
+                    sidebarId: "docs",
+                    position: "left",
+                    label: "Docs",
+                },
+                {
+                    label: "API docs",
+                    href: "https://yousif-khalil-abdulkarim.github.io/daiso-core/modules.html",
+                    position: "left",
+                },
+                {
+                    href: "https://github.com/yousif-khalil-abdulkarim/daiso-core/",
+                    label: "GitHub",
+                    position: "right",
+                },
+                {
+                    href: "https://www.npmjs.com/package/@daiso-tech/core",
+                    label: "NPM",
+                    position: "right",
+                },
+            ],
+        },
+        footer: {
+            style: "dark",
+            copyright: `© ${String(new Date().getFullYear())} @daiso-tech/core. Built with Docusaurus.`,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
+};
+
+export default config;
