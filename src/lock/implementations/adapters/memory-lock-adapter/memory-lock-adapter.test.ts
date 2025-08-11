39b8a901b1c1a60a07cc787a994b9b23406d7f0f
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2025-08-11 04:44:04 +0200

    Updated MemoryLockAdapter class

--- src/lock/implementations/adapters/memory-lock-adapter/memory-lock-adapter.test.ts
+++ src/lock/implementations/adapters/memory-lock-adapter/memory-lock-adapter.test.ts
@@ -1,13 +1,38 @@
-import { beforeEach, describe, expect, test } from "vitest";
+import { afterEach, beforeEach, describe, expect, test } from "vitest";
 import { lockAdapterTestSuite } from "@/lock/implementations/test-utilities/_module-exports.js";
-import { MemoryLockAdapter } from "@/lock/implementations/adapters/memory-lock-adapter/_module.js";
+import {
+    MemoryLockAdapter,
+    type MemoryLockData,
+} from "@/lock/implementations/adapters/memory-lock-adapter/_module.js";
+import { TimeSpan } from "@/utilities/_module-exports.js";
 
 describe("class: MemoryLockAdapter", () => {
+    let map = new Map<string, MemoryLockData>();
+    let adapter: MemoryLockAdapter;
+    beforeEach(() => {
+        map = new Map();
+        adapter = new MemoryLockAdapter(map);
+    });
+    afterEach(async () => {
+        await adapter.deInit();
+    });
     lockAdapterTestSuite({
-        createAdapter: () => new MemoryLockAdapter(new Map()),
+        createAdapter: () => adapter,
         test,
         beforeEach,
         expect,
         describe,
+    });
+    describe("method: deInit", () => {
+        test("Should remove table", async () => {
+            await adapter.acquire("a", "1", null);
+            await adapter.acquire("a", "2", TimeSpan.fromMilliseconds(100));
+            await adapter.acquire("b", "1", null);
+            await adapter.acquire("b", "2", TimeSpan.fromMilliseconds(100));
+
+            await adapter.deInit();
+
+            expect(map.size).toBe(0);
+        });
     });
 });
