7ba245b976f4f6e906b00f682c96ca7b25f4055c
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2025-08-27 02:44:29 +0200

    Added ISemaphoreAdapter contract

--- src/semaphore/contracts/semaphore-adapter.contract.ts
+++ src/semaphore/contracts/semaphore-adapter.contract.ts
@@ -19,6 +19,16 @@ export type SemaphoreAcquireSettings = {
 };
 
 /**
+ *
+ * IMPORT_PATH: `"@daiso-tech/core/semaphore/contracts"`
+ * @group Contracts
+ */
+export type ISemaphoreAdapterState = {
+    limit: number;
+    acquiredSlots: Map<string, Date | null>;
+};
+
+/**
  * The `ISemaphoreAdapter` contract defines a way for managing semaphores independent of the underlying technology.
  * This contract is not meant to be used directly, instead you should use {@link ISemaphoreProvider | `ISemaphoreProvider`} contract.
  *
@@ -35,13 +45,15 @@ export type ISemaphoreAdapter = {
 
     /**
      * The `release` method releases given slot related to the key.
+     *
+     * @returns Returns true if the semaphore exists and has at least one unavailable slot or false if all slots are available.
      */
-    release(key: string, slotId: string): Promise<void>;
+    release(key: string, slotId: string): Promise<boolean>;
 
     /**
      * The `forceReleaseAll` method releases all slots related to the key.
      */
-    forceReleaseAll(key: string): Promise<void>;
+    forceReleaseAll(key: string): Promise<boolean>;
 
     /**
      * The `refresh` method expiration of slot if not already expired.
@@ -49,4 +61,11 @@ export type ISemaphoreAdapter = {
      * @returns Returns true if the slot is refreshed* otherwise false is returned.
      */
     refresh(key: string, slotId: string, ttl: TimeSpan): Promise<boolean>;
+
+    /**
+     * The `getState` method returns the state of the semaphore.
+     *
+     * @returns Returns {@link ISemaphoreAdapterState | `ISemaphoreAdapterState`} if the semaphore exists in the database or null if doesnt exists.
+     */
+    getState(key: string): Promise<ISemaphoreAdapterState | null>;
 };
