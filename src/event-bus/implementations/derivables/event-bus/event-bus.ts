d988a4fab8eb4914ff0f594c2e6526990b94e877
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2025-09-27 22:26:41 +0200

    Formated the code

--- src/event-bus/implementations/derivables/event-bus/event-bus.ts
+++ src/event-bus/implementations/derivables/event-bus/event-bus.ts
@@ -182,7 +182,9 @@ export class EventBus<TEventMap extends BaseEventMap = BaseEventMap>
         listener: EventListener<TEventMap[TEventName]>,
     ): LazyPromise<void> {
         return this.createLazyPromise(async () => {
-            const key = this.namespace._internal_get().create(String(eventName));
+            const key = this.namespace
+                ._internal_get()
+                .create(String(eventName));
             const resolvedListener = this.store.getOrAdd(
                 key.namespaced,
                 listener,
@@ -205,7 +207,9 @@ export class EventBus<TEventMap extends BaseEventMap = BaseEventMap>
         listener: EventListener<TEventMap[TEventName]>,
     ): LazyPromise<void> {
         return this.createLazyPromise(async () => {
-            const key = this.namespace._internal_get().create(String(eventName));
+            const key = this.namespace
+                ._internal_get()
+                .create(String(eventName));
             const resolvedListener = this.store.getAndRemove(
                 key.namespaced,
                 listener,
@@ -255,7 +259,9 @@ export class EventBus<TEventMap extends BaseEventMap = BaseEventMap>
                 }
             };
 
-            const key = this.namespace._internal_get().create(String(eventName));
+            const key = this.namespace
+                ._internal_get()
+                .create(String(eventName));
             const resolvedListener = this.store.getOrAdd(
                 key.namespaced,
                 listener,
