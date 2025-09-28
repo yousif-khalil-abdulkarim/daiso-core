509486f03919eb5cb9403bb45ced569c2c7d7786
Yousif <yousif.khalil.abdulkarim.1999@gmail.com>
2025-09-28 16:34:38 +0200

    Removed unused classes

--- src/serde/implementations/adapters/_module-exports.ts
+++ src/serde/implementations/adapters/_module-exports.ts
@@ -1,3 +1,2 @@
-export * from "@/serde/implementations/adapters/mongodb-serde/_module.js";
 export * from "@/serde/implementations/adapters/no-op-serde-adapter/_module.js";
 export * from "@/serde/implementations/adapters/super-json-serde-adapter/_module.js";
