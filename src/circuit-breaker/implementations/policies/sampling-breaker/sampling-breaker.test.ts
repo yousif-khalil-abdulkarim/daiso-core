b132f5c15e6ef4c3a6e61b8743ddada35b296ea5
yousif-khalil-abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2025-11-27 00:23:12 +0100

    Added sampling-breaker policy

---
+++ src/circuit-breaker/implementations/policies/sampling-breaker/sampling-breaker.test.ts
@@ -0,0 +1,22 @@
+import { describe, test } from "vitest";
+
+describe("class: SamplingBreaker", () => {
+    describe("method: initialMetrics", () => {
+        test.todo("Write tests");
+    });
+    describe("method: whenClosed", () => {
+        test.todo("Write tests");
+    });
+    describe("method: whenHalfOpen", () => {
+        test.todo("Write tests");
+    });
+    describe("method: trackFailure", () => {
+        test.todo("Write tests");
+    });
+    describe("method: trackSuccess", () => {
+        test.todo("Write tests");
+    });
+    describe("method: isEqual", () => {
+        test.todo("Write tests");
+    });
+});
