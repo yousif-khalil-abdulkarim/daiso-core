302ff372ea24a3067c45948d512ca1aaba2061ef
yousif-khalil-abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2025-11-27 00:25:01 +0100

    Added CircuitBreakerProvider class

---
+++ src/circuit-breaker/implementations/derivables/circuit-breaker-provider/circuit-breaker-provider.ts
@@ -0,0 +1,234 @@
+/**
+ * @module CircuitBreaker
+ */
+
+import {
+    CIRCUIT_BREAKER_TRIGGER,
+    type CircuitBreakerEventMap,
+    type CircuitBreakerProviderCreateSettings,
+    type ICircuitBreaker,
+    type ICircuitBreakerProvider,
+    type ICircuitBreakerAdapter,
+    type CircuitBreakerTrigger,
+} from "@/circuit-breaker/contracts/_module-exports.js";
+import type {
+    EventListener,
+    IEventBus,
+    Unsubscribe,
+} from "@/event-bus/contracts/_module-exports.js";
+import { NoOpEventBusAdapter } from "@/event-bus/implementations/adapters/_module.js";
+import { EventBus } from "@/event-bus/implementations/derivables/_module-exports.js";
+import { Namespace } from "@/namespace/_module-exports.js";
+import type { Task } from "@/task/_module-exports.js";
+import { CircuitBreaker } from "@/circuit-breaker/implementations/derivables/circuit-breaker-provider/circuit-breaker.js";
+import type { ITimeSpan } from "@/time-span/contracts/_module-exports.js";
+import { TimeSpan } from "@/time-span/implementations/_module-exports.js";
+import {
+    CORE,
+    resolveOneOrMore,
+    type ErrorPolicy,
+    type ErrorPolicySettings,
+    type OneOrMore,
+} from "@/utilities/_module-exports.js";
+import type { ISerderRegister } from "@/serde/contracts/_module-exports.js";
+import { Serde } from "@/serde/implementations/derivables/serde.js";
+import { NoOpSerdeAdapter } from "@/serde/implementations/adapters/_module.js";
+import { CircuitBreakerSerdeTransformer } from "@/circuit-breaker/implementations/derivables/circuit-breaker-provider/circuit-breaker-serde-transformer.js";
+
+/**
+ *
+ * IMPORT_PATH: `"@daiso-tech/core/circuit-breaker"`
+ * @group Derivables
+ */
+export type CircuitBreakerProviderSettingsBase = ErrorPolicySettings & {
+    /**
+     * @default
+     * ```ts
+     * import { Namespace } from "@daiso-tech/core/namespace";
+     *
+     * new Namespace("@circuit-breaker")
+     * ```
+     */
+    namespace?: Namespace;
+
+    /**
+     * @default
+     * ```ts
+     * import { EventBus } from "@daiso-tech/core/event-bus";
+     * import { NoOpEventBusAdapter } from "@daiso-tech/core/event-bus/no-op-event-bus-adapter";
+     *
+     * new EventBus({
+     *   adapter: new NoOpEventBusAdapter()
+     * })
+     * ```
+     */
+    eventBus?: IEventBus;
+
+    /**
+     * @default
+     * ```ts
+     * import { TimeSpan } from "@daiso-tech/core/time-span";
+     *
+     * TimeSpan.fromSeconds(10);
+     * ```
+     */
+    slowCallTime?: ITimeSpan;
+
+    /**
+     * You can decide to track only errors, only slow calls or both as failures.
+     * @default
+     * ```ts
+     * import { CIRCUIT_BREAKER_TRIGGER} from "@daiso-tech/core/circuit-breaker/contracts";
+     *
+     * CIRCUIT_BREAKER_TRIGGER.BOTH
+     * ```
+     */
+    trigger?: CircuitBreakerTrigger;
+
+    serde?: OneOrMore<ISerderRegister>;
+
+    /**
+     * @default ""
+     */
+    serdeTransformerName?: string;
+};
+
+/**
+ *
+ * IMPORT_PATH: `"@daiso-tech/core/circuit-breaker"`
+ * @group Derivables
+ */
+export type CircuitBreakerProviderSettings =
+    CircuitBreakerProviderSettingsBase & {
+        adapter: ICircuitBreakerAdapter;
+    };
+
+/**
+ *
+ * IMPORT_PATH: `"@daiso-tech/core/circuit-breaker"`
+ * @group Derivables
+ */
+export const DEFAULT_CIRCUIT_BREAKER_PROVIDER_NAMESPACE = new Namespace(
+    "@circuit-breaker",
+);
+
+/**
+ * `CircuitBreakerProvider` class can be derived from any {@link ICircuitBreakerAdapter | `ICircuitBreakerAdapter`}.
+ *
+ * Note the {@link ICircuitBreaker | `ICircuitBreaker`} instances created by the `CircuitBreakerProvider` class are serializable and deserializable,
+ * allowing them to be seamlessly transferred across different servers, processes, and databases.
+ * This can be done directly using {@link ISerderRegister | `ISerderRegister`} or indirectly through components that rely on {@link ISerderRegister | `ISerderRegister`} internally.
+ *
+ * IMPORT_PATH: `"@daiso-tech/core/circuit-breaker"`
+ * @group Derivables
+ */
+export class CircuitBreakerProvider implements ICircuitBreakerProvider {
+    private readonly namespace: Namespace;
+    private readonly eventBus: IEventBus<CircuitBreakerEventMap>;
+    private readonly adapter: ICircuitBreakerAdapter;
+    private readonly slowCallTime: TimeSpan;
+    private readonly trigger: CircuitBreakerTrigger;
+    private readonly errorPolicy: ErrorPolicy;
+    private readonly serde: OneOrMore<ISerderRegister>;
+    private readonly serdeTransformerName: string;
+
+    constructor(settings: CircuitBreakerProviderSettings) {
+        const {
+            namespace = DEFAULT_CIRCUIT_BREAKER_PROVIDER_NAMESPACE,
+            eventBus = new EventBus({
+                adapter: new NoOpEventBusAdapter(),
+            }),
+            adapter,
+            slowCallTime = TimeSpan.fromSeconds(10),
+            trigger = CIRCUIT_BREAKER_TRIGGER.BOTH,
+            errorPolicy = () => true,
+            serde = new Serde(new NoOpSerdeAdapter()),
+            serdeTransformerName = "",
+        } = settings;
+
+        this.namespace = namespace;
+        this.eventBus = eventBus;
+        this.adapter = adapter;
+        this.slowCallTime = TimeSpan.fromTimeSpan(slowCallTime);
+        this.trigger = trigger;
+        this.errorPolicy = errorPolicy;
+        this.serde = serde;
+        this.serdeTransformerName = serdeTransformerName;
+        this.registerToSerde();
+    }
+
+    private registerToSerde(): void {
+        const transformer = new CircuitBreakerSerdeTransformer({
+            adapter: this.adapter,
+            slowCallTime: this.slowCallTime,
+            errorPolicy: this.errorPolicy,
+            trigger: this.trigger,
+            eventBus: this.eventBus,
+            namespace: this.namespace,
+            serdeTransformerName: this.serdeTransformerName,
+        });
+        for (const serde of resolveOneOrMore(this.serde)) {
+            serde.registerCustom(transformer, CORE);
+        }
+    }
+
+    addListener<TEventName extends keyof CircuitBreakerEventMap>(
+        eventName: TEventName,
+        listener: EventListener<CircuitBreakerEventMap[TEventName]>,
+    ): Task<void> {
+        return this.eventBus.addListener(eventName, listener);
+    }
+
+    removeListener<TEventName extends keyof CircuitBreakerEventMap>(
+        eventName: TEventName,
+        listener: EventListener<CircuitBreakerEventMap[TEventName]>,
+    ): Task<void> {
+        return this.eventBus.removeListener(eventName, listener);
+    }
+
+    listenOnce<TEventName extends keyof CircuitBreakerEventMap>(
+        eventName: TEventName,
+        listener: EventListener<CircuitBreakerEventMap[TEventName]>,
+    ): Task<void> {
+        return this.eventBus.listenOnce(eventName, listener);
+    }
+
+    asPromise<TEventName extends keyof CircuitBreakerEventMap>(
+        eventName: TEventName,
+    ): Task<CircuitBreakerEventMap[TEventName]> {
+        return this.eventBus.asPromise(eventName);
+    }
+
+    subscribeOnce<TEventName extends keyof CircuitBreakerEventMap>(
+        eventName: TEventName,
+        listener: EventListener<CircuitBreakerEventMap[TEventName]>,
+    ): Task<Unsubscribe> {
+        return this.eventBus.subscribeOnce(eventName, listener);
+    }
+
+    subscribe<TEventName extends keyof CircuitBreakerEventMap>(
+        eventName: TEventName,
+        listener: EventListener<CircuitBreakerEventMap[TEventName]>,
+    ): Task<Unsubscribe> {
+        return this.eventBus.subscribe(eventName, listener);
+    }
+
+    create(
+        key: string,
+        settings: CircuitBreakerProviderCreateSettings = {},
+    ): ICircuitBreaker {
+        const { errorPolicy = this.errorPolicy, trigger = this.trigger } =
+            settings;
+
+        return new CircuitBreaker({
+            eventDispatcher: this.eventBus,
+            adapter: this.adapter,
+            key: this.namespace.create(key),
+            slowCallTime: this.slowCallTime,
+            errorPolicy,
+            trigger,
+            serdeTransformerName: this.serdeTransformerName,
+            namespace: this.namespace,
+        });
+    }
+}
---
+++ src/circuit-breaker/implementations/derivables/circuit-breaker-provider/circuit-breaker.ts
@@ -0,0 +1,248 @@
+/**
+ * @module CircuitBreaker
+ */
+
+import {
+    type CircuitBreakerState,
+    type CircuitBreakerTrigger,
+    type ICircuitBreaker,
+    OpenCircuitBreakerError,
+    CIRCUIT_BREAKER_TRIGGER,
+    type ICircuitBreakerAdapter,
+    CIRCUIT_BREAKER_STATE,
+    type CircuitBreakerEventMap,
+    CIRCUIT_BREAKER_EVENTS,
+} from "@/circuit-breaker/contracts/_module-exports.js";
+import type { IEventDispatcher } from "@/event-bus/contracts/_module-exports.js";
+import type { Key, Namespace } from "@/namespace/_module-exports.js";
+import { Task } from "@/task/_module-exports.js";
+import { TimeSpan } from "@/time-span/implementations/_module-exports.js";
+import {
+    callErrorPolicyOnThrow,
+    resolveAsyncLazyable,
+    type AsyncLazy,
+    type ErrorPolicy,
+    type InvokableFn,
+} from "@/utilities/_module-exports.js";
+
+/**
+ * @internal
+ */
+export type CircuitBreakerSettings = {
+    eventDispatcher: IEventDispatcher<CircuitBreakerEventMap>;
+    adapter: ICircuitBreakerAdapter;
+    key: Key;
+    slowCallTime: TimeSpan;
+    errorPolicy: ErrorPolicy;
+    trigger: CircuitBreakerTrigger;
+    serdeTransformerName: string;
+    namespace: Namespace;
+};
+
+/**
+ * @internal
+ */
+export type ISerializedCircuitBreaker = {
+    version: "1";
+    key: string;
+};
+
+/**
+ * @internal
+ */
+export class CircuitBreaker implements ICircuitBreaker {
+    /**
+     * @internal
+     */
+    static _internal_serialize(
+        deserializedValue: CircuitBreaker,
+    ): ISerializedCircuitBreaker {
+        return {
+            version: "1",
+            key: deserializedValue._key.get(),
+        };
+    }
+
+    private readonly _key: Key;
+    private readonly errorPolicy: ErrorPolicy;
+    private readonly trigger: CircuitBreakerTrigger;
+    private readonly slowCallTime: TimeSpan;
+    private readonly adapter: ICircuitBreakerAdapter;
+    private readonly eventDispatcher: IEventDispatcher<CircuitBreakerEventMap>;
+    private readonly serdeTransformerName: string;
+    private readonly namespace: Namespace;
+
+    constructor(settings: CircuitBreakerSettings) {
+        const {
+            eventDispatcher,
+            key,
+            errorPolicy,
+            trigger,
+            adapter,
+            slowCallTime,
+            serdeTransformerName,
+            namespace,
+        } = settings;
+        this.eventDispatcher = eventDispatcher;
+        this._key = key;
+        this.errorPolicy = errorPolicy;
+        this.trigger = trigger;
+        this.adapter = adapter;
+        this.slowCallTime = slowCallTime;
+        this.serdeTransformerName = serdeTransformerName;
+        this.namespace = namespace;
+    }
+
+    _internal_getNamespace(): Namespace {
+        return this.namespace;
+    }
+
+    _internal_getSerdeTransformerName(): string {
+        return this.serdeTransformerName;
+    }
+
+    _internal_getAdapter(): ICircuitBreakerAdapter {
+        return this.adapter;
+    }
+
+    get key(): string {
+        return this._key.get();
+    }
+
+    getState(): Task<CircuitBreakerState> {
+        return new Task(async () => {
+            return this.adapter.getState(this._key.toString());
+        });
+    }
+
+    private async trackError<TValue = void>(
+        fn: InvokableFn<[], Promise<TValue>>,
+    ): Promise<TValue> {
+        try {
+            return await fn();
+        } catch (error: unknown) {
+            const isErrorMatching = await callErrorPolicyOnThrow(
+                this.errorPolicy,
+                error,
+            );
+            const shouldRecordError =
+                this.trigger === CIRCUIT_BREAKER_TRIGGER.BOTH ||
+                this.trigger === CIRCUIT_BREAKER_TRIGGER.ONLY_ERROR;
+
+            if (shouldRecordError && isErrorMatching) {
+                await this.adapter.trackFailure(this._key.toString());
+                this.eventDispatcher
+                    .dispatch(CIRCUIT_BREAKER_EVENTS.TRACKED_FAILURE, {
+                        circuitBreaker: this,
+                        error,
+                    })
+                    .detach();
+            } else if (shouldRecordError) {
+                this.eventDispatcher
+                    .dispatch(CIRCUIT_BREAKER_EVENTS.UNTRACKED_FAILURE, {
+                        circuitBreaker: this,
+                        error,
+                    })
+                    .detach();
+            }
+            throw error;
+        }
+    }
+
+    private async trackSlowCall<TValue = void>(
+        fn: InvokableFn<[], Promise<TValue>>,
+    ): Promise<TValue> {
+        const start = performance.now();
+
+        const value = await fn();
+
+        const end = performance.now();
+
+        const executionTime = TimeSpan.fromMilliseconds(end - start);
+
+        const shouldRecordSlowCall =
+            this.trigger === CIRCUIT_BREAKER_TRIGGER.BOTH ||
+            this.trigger === CIRCUIT_BREAKER_TRIGGER.ONLY_SLOW_CALL;
+        const isCallSlow = executionTime.gte(this.slowCallTime);
+
+        if (shouldRecordSlowCall && isCallSlow) {
+            await this.adapter.trackFailure(this._key.toString());
+            this.eventDispatcher
+                .dispatch(CIRCUIT_BREAKER_EVENTS.TRACKED_SLOW_CALL, {
+                    circuitBreaker: this,
+                })
+                .detach();
+        }
+        if (shouldRecordSlowCall && !isCallSlow) {
+            await this.adapter.trackSuccess(this._key.toString());
+            this.eventDispatcher
+                .dispatch(CIRCUIT_BREAKER_EVENTS.TRACKED_SUCCESS, {
+                    circuitBreaker: this,
+                })
+                .detach();
+        }
+        if (!shouldRecordSlowCall) {
+            await this.adapter.trackSuccess(this._key.toString());
+            this.eventDispatcher
+                .dispatch(CIRCUIT_BREAKER_EVENTS.TRACKED_SUCCESS, {
+                    circuitBreaker: this,
+                })
+                .detach();
+        }
+
+        return value;
+    }
+
+    private async guard(): Promise<void> {
+        const transition = await this.adapter.updateState(this._key.toString());
+        const hasStateChaned = transition.to !== transition.from;
+        if (hasStateChaned) {
+            this.eventDispatcher
+                .dispatch(CIRCUIT_BREAKER_EVENTS.STATE_TRANSITIONED, {
+                    circuitBreaker: this,
+                    from: transition.from,
+                    to: transition.to,
+                })
+                .detach();
+        }
+
+        const isInOpenState = transition.to === CIRCUIT_BREAKER_STATE.OPEN;
+        if (isInOpenState) {
+            throw new OpenCircuitBreakerError("!!__MESSAGE__!!");
+        }
+    }
+
+    runOrFail<TValue = void>(asyncFn: AsyncLazy<TValue>): Task<TValue> {
+        return new Task(async () => {
+            await this.guard();
+
+            return await this.trackError(async () => {
+                return await this.trackSlowCall(async () => {
+                    return await resolveAsyncLazyable(asyncFn);
+                });
+            });
+        });
+    }
+
+    reset(): Task<void> {
+        return new Task(async () => {
+            await this.adapter.reset(this._key.toString());
+            this.eventDispatcher
+                .dispatch(CIRCUIT_BREAKER_EVENTS.RESETED, {
+                    circuitBreaker: this,
+                })
+                .detach();
+        });
+    }
+
+    isolate(): Task<void> {
+        return new Task(async () => {
+            await this.adapter.isolate(this.key);
+            this.eventDispatcher
+                .dispatch(CIRCUIT_BREAKER_EVENTS.ISOLATED, {
+                    circuitBreaker: this,
+                })
+                .detach();
+        });
+    }
+}
