9e8b7add07db5b7eeccdb6440e5f2a044fa22c0b
yousif-khalil-abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2025-11-27 23:12:47 +0100

    Removed Result type from callErrorPolicyOnValue and callErrorPolicyOnThrow functions

--- src/utilities/functions/error-policy.test.ts
+++ src/utilities/functions/error-policy.test.ts
@@ -7,7 +7,6 @@ import {
 } from "@/utilities/functions/error-policy.js";
 import { z } from "zod";
 import type { IInvokableObject } from "@/utilities/functions/invokable.js";
-import { resultFailure, resultSuccess } from "@/utilities/functions/result.js";
 
 describe("file: error-policy.ts", () => {
     describe("function: isErrorPolicyBoolSetting", () => {
@@ -43,156 +42,24 @@ describe("file: error-policy.ts", () => {
         });
     });
     describe("function: callErrorPolicyOnValue", () => {
-        test("Should return true when ErrorPolicy is a class and given Result error is instance of the class", async () => {
-            class ErrorA extends Error {}
+        test("Should return false when ErrorPolicy ErrorPolicyBoolSetting.treatFalseAsError=false and given false value", () => {
             expect(
-                await callErrorPolicyOnValue(
-                    ErrorA,
-                    resultFailure(new ErrorA()),
-                ),
-            ).toBe(true);
-        });
-        test("Should return false when ErrorPolicy is a class and given Result error is not instance of the class", async () => {
-            class ErrorA extends Error {}
-            class ErrorB extends Error {}
-            expect(
-                await callErrorPolicyOnValue(
-                    ErrorA,
-                    resultFailure(new ErrorB()),
-                ),
+                callErrorPolicyOnValue({ treatFalseAsError: false }, false),
             ).toBe(false);
         });
-        test("Should return false when ErrorPolicy is a class and given Result error is not a class", async () => {
-            class ErrorA extends Error {}
+        test("Should return false when ErrorPolicy ErrorPolicyBoolSetting.treatFalseAsError=false and given true value", () => {
             expect(
-                await callErrorPolicyOnValue(ErrorA, resultFailure("str")),
+                callErrorPolicyOnValue({ treatFalseAsError: false }, true),
             ).toBe(false);
         });
-        test("Should return true when ErrorPolicy is a standard schema and given Result error matches the standard schema", async () => {
+        test("Should return false when ErrorPolicy ErrorPolicyBoolSetting.treatFalseAsError=true and given true value", () => {
             expect(
-                await callErrorPolicyOnValue(
-                    z.object({
-                        code: z.literal("e20"),
-                        message: z.string(),
-                    }),
-                    resultFailure({
-                        code: "e20",
-                        message: "Message",
-                    }),
-                ),
-            ).toBe(true);
-        });
-        test("Should return false when ErrorPolicy is a standard schema and given Result error matches not the standard schema", async () => {
-            expect(
-                await callErrorPolicyOnValue(
-                    z.object({
-                        code: z.literal("e20"),
-                        message: z.string(),
-                    }),
-                    resultFailure(new Error() as any),
-                ),
+                callErrorPolicyOnValue({ treatFalseAsError: true }, true),
             ).toBe(false);
         });
-        test("Should return true when ErrorPolicy is a predicate function and given Result error matches the predicate function", async () => {
+        test("Should return true when ErrorPolicy ErrorPolicyBoolSetting.treatFalseAsError=true and given false value", () => {
             expect(
-                await callErrorPolicyOnValue(
-                    (value) => value === "e20",
-                    resultFailure("e20"),
-                ),
-            ).toBe(true);
-        });
-        test("Should return false when ErrorPolicy is a predicate function and given Result error matches not the predicate function", async () => {
-            expect(
-                await callErrorPolicyOnValue(
-                    (value) => value === "e20",
-                    resultFailure("e40"),
-                ),
-            ).toBe(false);
-        });
-        test("Should return false when ErrorPolicy is a class and given Result success value", async () => {
-            class ErrorA extends Error {}
-            expect(
-                await callErrorPolicyOnValue(ErrorA, resultSuccess("str")),
-            ).toBe(false);
-        });
-        test("Should return false when ErrorPolicy is a standard schema and given Result success value", async () => {
-            expect(
-                await callErrorPolicyOnValue(
-                    z.object({
-                        code: z.literal("e20"),
-                        message: z.string(),
-                    }),
-                    resultSuccess("str"),
-                ),
-            ).toBe(false);
-        });
-        test("Should return false when ErrorPolicy is a predicate function and given Result success value", async () => {
-            expect(
-                await callErrorPolicyOnValue(
-                    (value) => value === "e20",
-                    resultSuccess("str"),
-                ),
-            ).toBe(false);
-        });
-        test("Should return false when ErrorPolicy ErrorPolicyBoolSetting.treatFalseAsError=false and given Result success value is false", async () => {
-            expect(
-                await callErrorPolicyOnValue(
-                    { treatFalseAsError: false },
-                    resultSuccess(false),
-                ),
-            ).toBe(false);
-        });
-        test("Should return false when ErrorPolicy ErrorPolicyBoolSetting.treatFalseAsError=false and given Result success value is true", async () => {
-            expect(
-                await callErrorPolicyOnValue(
-                    { treatFalseAsError: false },
-                    resultSuccess(true),
-                ),
-            ).toBe(false);
-        });
-        test("Should return false when ErrorPolicy ErrorPolicyBoolSetting.treatFalseAsError=true and given Result success value is true", async () => {
-            expect(
-                await callErrorPolicyOnValue(
-                    { treatFalseAsError: true },
-                    resultSuccess(true),
-                ),
-            ).toBe(false);
-        });
-        test("Should return false when ErrorPolicy ErrorPolicyBoolSetting.treatFalseAsError=true and given Result success value is false", async () => {
-            expect(
-                await callErrorPolicyOnValue(
-                    { treatFalseAsError: true },
-                    resultSuccess(false),
-                ),
-            ).toBe(false);
-        });
-        test("Should return false when ErrorPolicy ErrorPolicyBoolSetting.treatFalseAsError=false and given false value", async () => {
-            expect(
-                await callErrorPolicyOnValue(
-                    { treatFalseAsError: false },
-                    false,
-                ),
-            ).toBe(false);
-        });
-        test("Should return false when ErrorPolicy ErrorPolicyBoolSetting.treatFalseAsError=false and given true value", async () => {
-            expect(
-                await callErrorPolicyOnValue(
-                    { treatFalseAsError: false },
-                    true,
-                ),
-            ).toBe(false);
-        });
-        test("Should return false when ErrorPolicy ErrorPolicyBoolSetting.treatFalseAsError=true and given true value", async () => {
-            expect(
-                await callErrorPolicyOnValue({ treatFalseAsError: true }, true),
-            ).toBe(false);
-        });
-        test("Should return true when ErrorPolicy ErrorPolicyBoolSetting.treatFalseAsError=true and given false value", async () => {
-            expect(
-                await callErrorPolicyOnValue(
-                    { treatFalseAsError: true },
-                    false,
-                ),
+                callErrorPolicyOnValue({ treatFalseAsError: true }, false),
             ).toBe(true);
         });
     });
