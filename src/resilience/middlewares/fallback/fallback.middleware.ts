17c6712472f4f0bb0ba2bc4766b0e31bf21b70ab
yousif-khalil-abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2025-11-27 23:13:25 +0100

    Removed result type from fallback middleware

--- src/resilience/middlewares/fallback/fallback.middleware.ts
+++ src/resilience/middlewares/fallback/fallback.middleware.ts
@@ -3,13 +3,9 @@
  */
 
 import {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    type Result,
     resolveAsyncLazyable,
     callInvokable,
-    resultSuccess,
     callErrorPolicyOnValue,
-    isResultFailure,
 } from "@/utilities/_module-exports.js";
 import type { FallbackSettings } from "@/resilience/middlewares/fallback/fallback.types.js";
 import { callErrorPolicyOnThrow } from "@/utilities/_module-exports.js";
@@ -43,61 +39,32 @@ import type {
  * // Will return null when the fetch method throws an error.
  * console.log(await fetchData.invoke("URL_ENDPOINT"));
  * ```
- *
- * The middleware works also when the function returns a {@link Result | `Result`} type.
- * @example
- * ```ts
- * import { fallback } from "@daiso-tech/core/resilience";
- * import { AsyncHooks } from "@daiso-tech/core/hooks";
- * import { Result, resultFailure, resultSuccess } from "@daiso-tech/core/utilities";
- *
- * const fetchData = new AsyncHooks(async (url: string): Promise<Result> => {
- *   const response = await fetch(url);
- *   const json = await response.json();
- *   if (!response.ok) {
- *     return resultFailure(json);
- *   }
- *   return resultSuccess(json);
- * }, [
- *   fallback({ fallbackValue: null })
- * ]);
- *
- * // Will return null when the fetch method throws an error.
- * console.log(await fetchData.invoke("URL_ENDPOINT"));
- * ```
  */
 export function fallback<
     TParameters extends unknown[],
     TReturn,
     TContext extends HookContext,
 >(
-    settings: NoInfer<
-        FallbackSettings<TParameters, TReturn, TContext, TReturn>
-    >,
+    settings: NoInfer<FallbackSettings<TParameters, TReturn, TContext>>,
 ): AsyncMiddlewareFn<TParameters, TReturn, TContext> {
     const { fallbackValue, errorPolicy, onFallback = () => {} } = settings;
     return async (args, next, { context }): Promise<TReturn> => {
         try {
             const value = await next(...args);
 
-            // Handle fallback value if an Result type is returned
-            if (!(await callErrorPolicyOnValue(errorPolicy, value))) {
-                return value;
-            }
-            // This is only needed for type inference
-            if (!isResultFailure(value)) {
+            if (!callErrorPolicyOnValue(errorPolicy, value)) {
                 return value;
             }
 
             const resolvedFallbackValue =
                 await resolveAsyncLazyable(fallbackValue);
             callInvokable(onFallback, {
-                error: value.error,
-                fallbackValue: resolvedFallbackValue as TReturn,
+                error: value,
+                fallbackValue: resolvedFallbackValue,
                 args,
                 context,
             });
-            return resultSuccess(resolvedFallbackValue) as TReturn;
+            return resolvedFallbackValue;
 
             // Handle fallback value if an error is thrown
         } catch (error: unknown) {
