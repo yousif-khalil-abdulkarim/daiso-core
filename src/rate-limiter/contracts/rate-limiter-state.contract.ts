bc94f551246e67693f7f63f61e2bf6ce16e22a2e
yousif-khalil-abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2025-12-09 00:35:20 +0100

    Added RateLimiterState contract

---
+++ src/rate-limiter/contracts/rate-limiter-state.contract.ts
@@ -0,0 +1,57 @@
+/**
+ * @module RateLimiter
+ */
+
+import type { ITimeSpan } from "@/time-span/contracts/_module-exports.js";
+
+/**
+ *
+ * IMPORT_PATH: `"@daiso-tech/core/rate-limiter/contracts"`
+ * @group Contracts
+ */
+export const RATE_LIMITER_STATE = {
+    BLOCKED: "BLOCKED",
+    ALLOWED: "ALLOWED",
+} as const;
+
+/**
+ *
+ * IMPORT_PATH: `"@daiso-tech/core/rate-limiter/contracts"`
+ * @group Contracts
+ */
+export type RateLimiterStateLiterals =
+    (typeof RATE_LIMITER_STATE)[keyof typeof RATE_LIMITER_STATE];
+
+/**
+ *
+ * IMPORT_PATH: `"@daiso-tech/core/rate-limiter/contracts"`
+ * @group Contracts
+ */
+export type RateLimiterAllowedState = {
+    type: (typeof RATE_LIMITER_STATE)["ALLOWED"];
+    usedAttempts: number;
+    reaminingAttemps: number;
+    limit: number;
+};
+
+/**
+ *
+ * IMPORT_PATH: `"@daiso-tech/core/rate-limiter/contracts"`
+ * @group Contracts
+ */
+export type RateLimiterBlockedState = {
+    type: (typeof RATE_LIMITER_STATE)["BLOCKED"];
+    limit: number;
+    totalAttempts: number;
+    exceedAttempts: number;
+    resetTime: ITimeSpan;
+};
+
+/**
+ *
+ * IMPORT_PATH: `"@daiso-tech/core/rate-limiter/contracts"`
+ * @group Contracts
+ */
+export type RateLimiterState =
+    | RateLimiterBlockedState
+    | RateLimiterAllowedState;
