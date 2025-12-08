23b7543d1ed251d276028628a853dc04e8d62476
yousif-khalil-abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2025-12-09 00:49:37 +0100

    Added MongodbRateLimiterStorageAdapter

---
+++ src/rate-limiter/implementations/adapters/mongodb-rate-limiter-storage-adapter/mongodb-rate-limiter-storage-adapter.ts
@@ -0,0 +1,200 @@
+/**
+ * @module RateLimiter
+ */
+
+import type {
+    IRateLimiterData,
+    IRateLimiterStorageAdapter,
+    IRateLimiterStorageAdapterTransaction,
+} from "@/rate-limiter/contracts/_module-exports.js";
+import type { ISerde } from "@/serde/contracts/serde.contract.js";
+import type {
+    IDeinitizable,
+    IInitizable,
+    InvokableFn,
+} from "@/utilities/_module-exports.js";
+import type {
+    Collection,
+    CollectionOptions,
+    Db,
+    MongoClient,
+    ObjectId,
+} from "mongodb";
+
+/**
+ * IMPORT_PATH: `"@daiso-tech/core/rate-limiter/mongodb-rate-limiter-storage-adapter"`
+ * @group Adapters
+ */
+export type MongodbRateLimiterStorageAdapterSettings = {
+    client: MongoClient;
+    database: Db;
+    /**
+     * @default "rateLimiter"
+     */
+    collectionName?: string;
+    collectionSettings?: CollectionOptions;
+    serde: ISerde<string>;
+};
+
+/**
+ * IMPORT_PATH: `"@daiso-tech/core/rate-limiter/mongodb-rate-limiter-storage-adapter"`
+ * @group Adapters
+ */
+export type MongodbRateLimiterDocument = {
+    _id: ObjectId;
+    key: string;
+    state: string;
+    expiration: Date | null;
+};
+
+/**
+ * IMPORT_PATH: `"@daiso-tech/core/rate-limiter/mongodb-rate-limiter-storage-adapter"`
+ * @group Adapters
+ */
+export class MongodbRateLimiterStorageAdapter<TType>
+    implements IRateLimiterStorageAdapter<TType>, IInitizable, IDeinitizable
+{
+    private readonly client: MongoClient;
+    private readonly collection: Collection<MongodbRateLimiterDocument>;
+    private readonly serde: ISerde<string>;
+
+    /**
+     * @example
+     * ```ts
+     * import { MongodbRateLimiterStorageAdapter } from "@daiso-tech/core/rate-limiter/mongodb-rate-limiter-storage-adapter";
+     * import { MongoClient } from "mongodb";
+     * import { Serde } from "@daiso-tech/core/serde";
+     * import { SuperJsonSerdeAdapter } from "@daiso-tech/core/serde/super-json-serde-adapter"
+     *
+     * const client = await MongoClient.connect("YOUR_MONGODB_CONNECTION_STRING");
+     * const database = client.db("database");
+     * const serde = new Serde(new SuperJsonSerdeAdapter());
+     * const rateLimiterStorageAdapter = new MongodbRateLimiterStorageAdapter({
+     *   client,
+     *   database,
+     *   serde
+     * });
+     * // You need initialize the adapter once before using it.
+     * await rateLimiterStorageAdapter.init()
+     * ```
+     */
+    constructor(settings: MongodbRateLimiterStorageAdapterSettings) {
+        const {
+            client,
+            collectionName = "rateLimiter",
+            collectionSettings,
+            database,
+            serde,
+        } = settings;
+        this.client = client;
+        this.collection = database.collection(
+            collectionName,
+            collectionSettings,
+        );
+        this.serde = serde;
+    }
+
+    /**
+     * Removes the collection where the rate limiter keys are stored and all it's related indexes.
+     * Note all rate limiter data will be removed.
+     */
+    async deInit(): Promise<void> {
+        // Should throw if the collection already does not exists thats why the try catch is used.
+        try {
+            await this.collection.dropIndexes();
+        } catch {
+            /* EMPTY */
+        }
+
+        // Should throw if the collection already does not exists thats why the try catch is used.
+        try {
+            await this.collection.drop();
+        } catch {
+            /* EMPTY */
+        }
+    }
+
+    /**
+     * Creates all related indexes.
+     * Note the `init` method needs to be called once before using the adapter.
+     */
+    async init(): Promise<void> {
+        // Should throw if the index already exists thats why the try catch is used.
+        try {
+            await this.collection.createIndex(
+                {
+                    key: 1,
+                },
+                {
+                    unique: true,
+                },
+            );
+        } catch {
+            /* EMPTY */
+        }
+
+        // Should throw if the index already exists thats why the try catch is used.
+        try {
+            await this.collection.createIndex("expiration", {
+                expireAfterSeconds: 0,
+            });
+        } catch {
+            /* EMPTY */
+        }
+    }
+
+    private async upsert(
+        key: string,
+        state: TType,
+        expiration: Date | null,
+    ): Promise<void> {
+        await this.collection.updateOne(
+            {
+                key,
+            },
+            {
+                $set: {
+                    state: this.serde.serialize(state),
+                    expiration,
+                },
+            },
+            {
+                upsert: true,
+            },
+        );
+    }
+
+    async transaction<TValue>(
+        fn: InvokableFn<
+            [transaction: IRateLimiterStorageAdapterTransaction<TType>],
+            Promise<TValue>
+        >,
+    ): Promise<TValue> {
+        return await this.client.startSession().withTransaction(async () => {
+            return await fn({
+                upsert: (key, state, exiration) =>
+                    this.upsert(key, state, exiration),
+                find: (key) => this.find(key),
+            });
+        });
+    }
+
+    async find(key: string): Promise<IRateLimiterData<TType> | null> {
+        const doc = await this.collection.findOne({
+            key,
+        });
+        if (doc === null) {
+            return null;
+        }
+        return {
+            state: this.serde.deserialize(doc.state),
+            expiration: doc.expiration,
+        };
+    }
+
+    async remove(key: string): Promise<void> {
+        await this.collection.deleteOne({
+            key,
+        });
+    }
+}
