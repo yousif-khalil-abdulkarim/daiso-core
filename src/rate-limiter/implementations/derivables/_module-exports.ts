058842a2f0f73279c98783d09d018158400621d0
yousif-khalil-abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2025-12-09 00:48:24 +0100

    Added _module-exports.ts file for rate limiter derivables

---
+++ src/rate-limiter/implementations/derivables/_module-exports.ts
@@ -0,0 +1,2 @@
+export * from "@/rate-limiter/implementations/derivables/rate-limiter-provider/_module.js";
+export * from "@/rate-limiter/implementations/derivables/rate-limiter-provider-factory/_module.js";
