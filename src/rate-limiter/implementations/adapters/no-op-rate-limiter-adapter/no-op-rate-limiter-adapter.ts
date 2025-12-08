e2e1580f3a5ca09ab57234f4b92111a394b6b278
yousif-khalil-abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2025-12-09 00:49:26 +0100

    Added NoOpRateLimiterAdapter

---
+++ src/rate-limiter/implementations/adapters/no-op-rate-limiter-adapter/no-op-rate-limiter-adapter.ts
@@ -0,0 +1,39 @@
+/**
+ * @module RateLimiter
+ */
+
+import type {
+    IRateLimiterAdapter,
+    IRateLimiterAdapterState,
+} from "@/rate-limiter/contracts/_module-exports.js";
+
+/**
+ * IMPORT_PATH: `"@daiso-tech/core/rate-limiter/no-op-rate-limiter-adapter"`
+ * @group Adapters
+ */
+export class NoOpRateLimiterAdapter implements IRateLimiterAdapter {
+    getState(_key: string, limit: number): Promise<IRateLimiterAdapterState> {
+        return Promise.resolve({
+            success: true,
+            attempt: 1,
+            limit,
+            resetTime: null,
+        });
+    }
+
+    updateState(
+        _key: string,
+        limit: number,
+    ): Promise<IRateLimiterAdapterState> {
+        return Promise.resolve({
+            success: true,
+            attempt: 1,
+            limit,
+            resetTime: null,
+        });
+    }
+
+    reset(_key: string): Promise<void> {
+        return Promise.resolve();
+    }
+}
