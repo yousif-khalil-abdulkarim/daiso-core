6b6a9013ef8b0c37aaf427965a32a652d5d4eaf7
yousif-khalil-abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2025-12-09 00:35:55 +0100

    Added IRateLimiterProviderFactory contract

---
+++ src/rate-limiter/contracts/rate-limiter-provider-factory.contract.ts
@@ -0,0 +1,27 @@
+/**
+ * @module RateLimiter
+ */
+
+import type { IRateLimiterProvider } from "@/rate-limiter/contracts/rate-limiter-provider.contract.js";
+import {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    UnregisteredAdapterError,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    DefaultAdapterNotDefinedError,
+} from "@/utilities/_module-exports.js";
+
+/**
+ * The `IRateLimiterProviderFactory` contract makes it easy to configure and switch between different {@link IRateLimiterProvider | `IRateLimiterProvider`} dynamically.
+ *
+ * IMPORT_PATH: `"@daiso-tech/core/rate-limiter/contracts"`
+ * @group Contracts
+ */
+export type IRateLimiterProviderFactory<TAdapters extends string = string> = {
+    /**
+     * The `use` method will throw an error if you provide it unregisted adapter.
+     * If no default adapter is defined an error will be thrown by `use` method.
+     * @throws {UnregisteredAdapterError} {@link UnregisteredAdapterError}
+     * @throws {DefaultAdapterNotDefinedError} {@link DefaultAdapterNotDefinedError}
+     */
+    use(adapterName?: TAdapters): IRateLimiterProvider;
+};
