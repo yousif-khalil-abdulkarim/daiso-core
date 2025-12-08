c783178cddb1a67064f13e74a416c60165f71475
yousif-khalil-abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2025-12-09 00:35:47 +0100

    Added IRateLimiterProvider contract

---
+++ src/rate-limiter/contracts/rate-limiter-provider.contract.ts
@@ -0,0 +1,47 @@
+/**
+ * @module RateLimiter
+ */
+
+import type { ErrorPolicySettings } from "@/utilities/_module-exports.js";
+import type { IRateLimiter } from "@/rate-limiter/contracts/rate-limiter.contract.js";
+import type { IEventListenable } from "@/event-bus/contracts/_module-exports.js";
+import type { RateLimiterEventMap } from "@/rate-limiter/contracts/rate-limiter.events.js";
+
+/**
+ * IMPORT_PATH: `"@daiso-tech/core/rate-limiter/contracts"`
+ * @group Contracts
+ */
+export type RateLimiterProviderCreateSettings = ErrorPolicySettings & {
+    /**
+     * If true will only apply rate limiting when function errors and not when function is called.
+     * @default false
+     */
+    onlyError?: boolean;
+
+    limit: number;
+};
+
+/**
+ * IMPORT_PATH: `"@daiso-tech/core/rate-limiter/contracts"`
+ * @group Contracts
+ */
+export type IRateLimiterProviderBase = {
+    create(
+        key: string,
+        settings: RateLimiterProviderCreateSettings,
+    ): IRateLimiter;
+};
+
+/**
+ * IMPORT_PATH: `"@daiso-tech/core/rate-limiter/contracts"`
+ * @group Contracts
+ */
+export type IRateLimiterListenable = IEventListenable<RateLimiterEventMap>;
+
+/**
+ *
+ * IMPORT_PATH: `"@daiso-tech/core/rate-limiter/contracts"`
+ * @group Contracts
+ */
+export type IRateLimiterProvider = IRateLimiterListenable &
+    IRateLimiterProviderBase;
