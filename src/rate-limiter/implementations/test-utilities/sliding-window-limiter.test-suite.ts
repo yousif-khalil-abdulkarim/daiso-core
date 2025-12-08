8b05c798fb45baf1dedaf892fdf6dfc22d575851
yousif-khalil-abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2025-12-09 00:39:26 +0100

    Added unimplemented slidingWindowLimiterTestSuite helper function

---
+++ src/rate-limiter/implementations/test-utilities/sliding-window-limiter.test-suite.ts
@@ -0,0 +1,120 @@
+/**
+ * @module RateLimiter
+ */
+
+import { TimeSpan } from "@/time-span/implementations/_module-exports.js";
+import { Task } from "@/task/_module-exports.js";
+import {
+    type TestAPI,
+    type SuiteAPI,
+    type ExpectStatic,
+    type beforeEach,
+} from "vitest";
+import { type IRateLimiterAdapter } from "@/rate-limiter/contracts/_module-exports.js";
+import { type Promisable } from "@/utilities/_module-exports.js";
+import type { SlidingWindowLimiterSettings } from "@/rate-limiter/implementations/policies/_module-exports.js";
+import {
+    BACKOFFS,
+    type ConstantBackoffSettingsEnum,
+} from "@/backoff-policies/_module-exports.js";
+
+/**
+ *
+ * IMPORT_PATH: `"@daiso-tech/core/rate-limiter/test-utilities"`
+ * @group TestUtilities
+ */
+export type SlidingWindowLimiterTestSuiteSettings = {
+    expect: ExpectStatic;
+    test: TestAPI;
+    describe: SuiteAPI;
+    beforeEach: typeof beforeEach;
+    createAdapter: () => Promisable<IRateLimiterAdapter>;
+};
+
+/**
+ * @group TestUtilities
+ */
+const rateLimiterPolicySettings: Required<SlidingWindowLimiterSettings> = {
+    attempts: 5,
+    timeSpan: TimeSpan.fromMilliseconds(100),
+};
+
+/**
+ * @group TestUtilities
+ */
+const backoffPolicySettings: Required<ConstantBackoffSettingsEnum> = {
+    type: BACKOFFS.CONSTANT,
+    delay: TimeSpan.fromMilliseconds(50),
+    jitter: 0.5,
+    enableJitter: false,
+};
+
+/**
+ * IMPORT_PATH: `"@daiso-tech/core/rate-limiter/test-utilities"`
+ * @group TestUtilities
+ *
+ * @example
+ * ```ts
+ * import { beforeEach, describe, expect, test } from "vitest";
+ * import { DatabaseRateLimiterAdapter } from "@daiso-tech/core/rate-limiter/database-rate-limiter-adapter";
+ * import { SlidingWindowLimiter } from "@daiso-tech/core/rate-limiter/policies";
+ * import { slidingWindowLimiterTestSuite } from "@daiso-tech/core/rate-limiter/test-utilities";
+ * import { constantBackoff } from "@daiso-tech/core/backoff-policies";
+ * import { MemoryRateLimiterStorageAdapter } from "@daiso-tech/core/rate-limiter/memory-rate-limiter-storage-adapter";
+ *
+ * describe("sliding-window-limiter class: DatabaseRateLimiterAdapter", () => {
+ *     slidingWindowLimiterTestSuite({
+ *         createAdapter: () => {
+ *             const adapter = new DatabaseRateLimiterAdapter({
+ *                 adapter: new MemoryRateLimiterStorageAdapter(),
+ *                 backoffPolicy: constantBackoff(
+ *                     slidingWindowLimiterTestSuite.backoffPolicySettings,
+ *                 ),
+ *                 rateLimiterPolicy: new SlidingWindowLimiter(
+ *                     slidingWindowLimiterTestSuite.rateLimiterPolicySettings,
+ *                 ),
+ *             });
+ *             return adapter;
+ *         },
+ *         beforeEach,
+ *         describe,
+ *         expect,
+ *         test,
+ *     });
+ * });
+ * ```
+ */
+export function slidingWindowLimiterTestSuite(
+    settings: SlidingWindowLimiterTestSuiteSettings,
+): void {
+    const { expect, test, createAdapter, describe, beforeEach } = settings;
+    let adapter: IRateLimiterAdapter;
+    const waitTime = TimeSpan.fromTimeSpan(backoffPolicySettings.delay);
+    describe("Reusable tests:", () => {
+        beforeEach(async () => {
+            adapter = await createAdapter();
+        });
+
+        const KEY = "a";
+        async function delay(timeSpan: TimeSpan): Promise<void> {
+            await Task.delay(timeSpan);
+        }
+
+        describe("method: getState", () => {
+            test.todo("Write tests!!!");
+        });
+        describe("method: updateState / trackFailure / trackSuccess", () => {
+            test.todo("Write tests!!!");
+        });
+        describe("method: updateState / trackFailure / isolate / getState", () => {
+            test.todo("Write tests!!!");
+        });
+        describe("method: updateState / trackFailure / reset", () => {
+            test.todo("Write tests!!!");
+        });
+    });
+}
+
+slidingWindowLimiterTestSuite.rateLimiterPolicySettings =
+    rateLimiterPolicySettings;
+slidingWindowLimiterTestSuite.backoffPolicySettings = backoffPolicySettings;
