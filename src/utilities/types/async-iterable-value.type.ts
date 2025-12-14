3a9c8b4b546bc4d91f45d67a2471ac069ad5c17a
yousif-khalil-abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2025-12-14 21:56:45 +0100

    Added new IterableValue type and updated AsyncIterableValue type.

--- src/utilities/types/async-iterable-value.type.ts
+++ src/utilities/types/async-iterable-value.type.ts
@@ -6,6 +6,14 @@
  *
  * IMPORT_PATH: `"@daiso-tech/core/utilities"`
  */
-export type AsyncIterableValue<TInput> =
+export type IterableValue<TInput = unknown> =
     | Iterable<TInput>
+    | ArrayLike<TInput>;
+
+/**
+ *
+ * IMPORT_PATH: `"@daiso-tech/core/utilities"`
+ */
+export type AsyncIterableValue<TInput = unknown> =
+    | IterableValue<TInput>
     | AsyncIterable<TInput>;
