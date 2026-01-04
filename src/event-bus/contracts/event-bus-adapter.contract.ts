dd14d87e62ae8844b7665506dc8a83b4e566ca25
yousif-khalil-abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2026-01-04 22:17:16 +0100

    Added new event bus adapter contracts

--- src/event-bus/contracts/event-bus-adapter.contract.ts
+++ src/event-bus/contracts/event-bus-adapter.contract.ts
@@ -19,13 +19,23 @@ export type BaseEvent = Record<string, unknown>;
 export type EventListenerFn<TEvent> = InvokableFn<[event: TEvent]>;
 
 /**
- * The `IEventBusAdapter` contract defines a way for dispatching and listening to events independent of underlying technology.
- * This contract is not meant to be used directly, instead you should use `IEventBus`
  *
  * IMPORT_PATH: `"@daiso-tech/core/event-bus/contracts"`
  * @group Contracts
  */
-export type IEventBusAdapter = {
+export type IEventBusDispatcherAdapter = {
+    /**
+     * The `dispatch` method is used for dispatching one or multiple `events`.
+     */
+    dispatch(eventName: string, eventData: BaseEvent): Promise<void>;
+};
+
+/**
+ *
+ * IMPORT_PATH: `"@daiso-tech/core/event-bus/contracts"`
+ * @group Contracts
+ */
+export type IEventBusListenableAdapter = {
     /**
      * The `addListener` method is used for adding {@link EventListenerFn | `listener`} for certain `eventName`.
      */
@@ -41,9 +51,14 @@ export type IEventBusAdapter = {
         eventName: string,
         listener: EventListenerFn<BaseEvent>,
     ): Promise<void>;
-
-    /**
-     * The `dispatch` method is used for dispatching one or multiple `events`.
-     */
-    dispatch(eventName: string, eventData: BaseEvent): Promise<void>;
 };
+
+/**
+ * The `IEventBusAdapter` contract defines a way for dispatching and listening to events independent of underlying technology.
+ * This contract is not meant to be used directly, instead you should use `IEventBus`
+ *
+ * IMPORT_PATH: `"@daiso-tech/core/event-bus/contracts"`
+ * @group Contracts
+ */
+export type IEventBusAdapter = IEventBusDispatcherAdapter &
+    IEventBusListenableAdapter;
