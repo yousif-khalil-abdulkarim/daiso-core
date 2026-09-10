6ff108ae2bf58dc49f0770b746cae6b883c4736b
Yousif Abdulkarim <yousif.khalil.abdulkarim.1999@gmail.com>
2026-09-10 22:13:33 +0200

    docs(time-span): use extensioned imports in time span samples

--- website/docs/components/time_span/samples/subtracting_time_span.ts
+++ website/docs/components/time_span/samples/subtracting_time_span.ts
@@ -1,4 +1,4 @@
-import { timeSpan } from "./creating_from_milliseconds";
+import { timeSpan } from "./creating_from_milliseconds.js";
 import { TimeSpan } from "eridu-tech/time-span";
 
 timeSpan.subtractTimeSpan(TimeSpan.fromDays(14).addHours(20));
